ac0a171a77bf72f9aecbf0268bcd473ee72f9421
Steven Ru <z3418434@unsw.edu.au>
2014-09-27 21:34:44 +1000

    Parameter checking for booking a ticket and checking out

--- 9321Ass2/src/edu/unsw/comp9321Ass2/logic/BookTicketCommand.java
+++ 9321Ass2/src/edu/unsw/comp9321Ass2/logic/BookTicketCommand.java
@@ -20,16 +20,27 @@ public class BookTicketCommand implements Command {
 		String forwardPage;
 		if(cast.checkLogin((String)request.getSession().getAttribute("userSess"),(String)request.getSession().getAttribute("passSess"))) {
 			int movieID = Integer.parseInt(request.getParameter("movieID"));
-			int amount = Integer.parseInt(request.getParameter("ticket amount"));
+			String amountStr = request.getParameter("ticket amount");
+			int amount;
+			if(amountStr != null && !amountStr.equals("") && amountStr.matches("^\\d*$")){
+				amount = Integer.parseInt(amountStr);
+			}else{
+				amount = 0;
+			}
 			int showtimeID = Integer.parseInt(request.getParameter("showtimeID"));
 			//If overbook don't accept booking
-			if(cast.findMovieShowtimebyID(showtimeID).getBooked() + amount <= cast.findCinemaByID(cast.findMovieShowtimebyID(showtimeID).getCinemaID()).getCapacity()) {
+			if(amount > 0 && cast.findMovieShowtimebyID(showtimeID).getBooked() + amount <= cast.findCinemaByID(cast.findMovieShowtimebyID(showtimeID).getCinemaID()).getCapacity()) {
 				int userID = cast.findUser((String)request.getSession().getAttribute("userSess")).getID();
 				request.setAttribute("showtimeID", showtimeID);
 				request.setAttribute("amount", amount);
 				request.setAttribute("userID", userID);
 				forwardPage = "checkout.jsp";
-			} else {
+			}else{
+				if(amount == 0){
+					request.getSession().setAttribute("message","Please enter how many tickets you need.");
+				}else{
+					request.getSession().setAttribute("message","Not enough seat available");
+				}
 				MovieDTO movie = cast.findMovieByID(movieID);
 				List<ShowtimeDTO> showtimeList = cast.findMovieShowtimebyMovieID(movieID);
 				List<CinemaDTO> cinemaList = new ArrayList<CinemaDTO>();
@@ -40,7 +51,6 @@ public class BookTicketCommand implements Command {
 				request.setAttribute("showtimeList", showtimeList);
 				request.setAttribute("cinemaList", cinemaList);
 				request.setAttribute("movieID",movieID);
-				request.getSession().setAttribute("message","Not enough seat available");
 				forwardPage = "checkMovieShowtime.jsp";
 			}
 		} else {
--- 9321Ass2/src/edu/unsw/comp9321Ass2/logic/CheckoutCommand.java
+++ 9321Ass2/src/edu/unsw/comp9321Ass2/logic/CheckoutCommand.java
@@ -14,28 +14,36 @@ public class CheckoutCommand implements Command {
 		
 		String forwardPage;
 		if(cast.checkLogin((String)request.getSession().getAttribute("userSess"),(String)request.getSession().getAttribute("passSess"))) {
-			int amount = Integer.parseInt(request.getParameter("amount"));
-			int showtimeID = Integer.parseInt(request.getParameter("showtimeID"));
-			String cardName = request.getParameter("cardName");
-			String cardNum = request.getParameter("cardNo");
-			int cardCSC = Integer.parseInt(request.getParameter("cardCSC"));
 			
-			if(cast.findMovieShowtimebyID(showtimeID).getBooked() + amount <= 
-			   cast.findCinemaByID(cast.findMovieShowtimebyID(showtimeID).getCinemaID()).getCapacity()
-			   && checkParams(request).equals("Success")){
-				int userID = cast.findUser((String)request.getSession().getAttribute("userSess")).getID();
-				cast.setBooked(showtimeID, amount);
-				cast.addBooking(showtimeID, userID, amount,cardName,cardNum,cardCSC);
-				request.getSession().setAttribute("message", "Booking completed. You can view your bookings via the home page.");
-				forwardPage = "success.jsp";
-			} else {
-				if(checkParams(request).matches("Success")){
-					request.getSession().setAttribute("message", "Problem occured in booking: bookings are sold out.");
-				}else{
-					request.getSession().setAttribute("message", "Problem occured in booking:"+ checkParams(request));
+			if(checkParams(request).equals("Success")){
+				int amount = Integer.parseInt(request.getParameter("amount"));
+				int showtimeID = Integer.parseInt(request.getParameter("showtimeID"));
+				String cardName = request.getParameter("cardName");
+				String cardNum = request.getParameter("cardNo");
+				int cardCSC = Integer.parseInt(request.getParameter("cardCSC"));
+			
+				if(cast.findMovieShowtimebyID(showtimeID).getBooked() + amount <= 
+				   cast.findCinemaByID(cast.findMovieShowtimebyID(showtimeID).getCinemaID()).getCapacity()
+				   && checkParams(request).equals("Success")){
+					int userID = cast.findUser((String)request.getSession().getAttribute("userSess")).getID();
+					cast.setBooked(showtimeID, amount);
+					cast.addBooking(showtimeID, userID, amount,cardName,cardNum,cardCSC);
+					request.getSession().setAttribute("message", "Booking completed. You can view your bookings via the home page.");
+					forwardPage = "success.jsp";
+				} else {
+					if(checkParams(request).matches("Success")){
+						request.getSession().setAttribute("message", "Problem occured in booking: bookings are sold out.");
+					}else{
+						request.getSession().setAttribute("message", "Problem occured in booking:"+ checkParams(request));
+					}
+					forwardPage = "home.jsp";
 				}
+			}else{
+				request.getSession().setAttribute("message", "Problem occured in booking:"+ checkParams(request));
 				forwardPage = "home.jsp";
 			}
+			
+			
 		} else {
 			forwardPage = "reject1.jsp";
 		}
@@ -49,7 +57,7 @@ public class CheckoutCommand implements Command {
 		String cardCSC = (String) request.getParameter("cardCSC");
 		
 		
-		if(cardName == null){
+		if(cardName == null || cardName.equals("")){
 			output = "Please enter a card name.";
 		}else if(cardNo == null){
 			output = "Please enter a card number.";
